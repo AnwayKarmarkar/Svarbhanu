70fad7575eb554b52ae0ad38856addead5503412
Courier6 <anway.karmarkar@gmail.com>
2024-08-04 11:11:12 -0400

    setup

---
+++ svarbhanu/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Assets.Scripts.PlayerScripts {
+    public class PlayerMovement : MonoBehaviour {
+        public Rigidbody2D Rb;
+        public float MovementSpeed = 5f;
+
+        private Vector2 _movement;
+        private Vector2 _mousePos;
+        public Camera Camera;
+
+        // Update is called once per frame
+        void Update() {
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.y = Input.GetAxisRaw("Vertical");
+
+            _mousePos = Camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        public void FixedUpdate() {
+            Rb.MovePosition(Rb.position + _movement * MovementSpeed * Time.fixedDeltaTime);
+
+            var lookDir = _mousePos - Rb.position;
+            var angle = Mathf.Atan2(lookDir.y, lookDir.x);
+        }
+    }
+}
