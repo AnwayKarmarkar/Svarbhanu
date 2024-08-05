c9cdb56da8163979ef6baa6df5fb515e4ffcae81
Courier6 <anway.karmarkar@gmail.com>
2024-08-05 00:02:26 -0400

    Added player dash, created test environment with training dummy

--- svarbhanu/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ svarbhanu/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Assets.Scripts.PlayerScripts {
@@ -9,14 +11,40 @@ namespace Assets.Scripts.PlayerScripts {
         private Vector2 _mousePos;
         public Camera Camera;
 
+        [Header("DashSettings")] 
+        [SerializeField]
+        private float _dashDistance = 3f;
+        [SerializeField]
+        private float _dashCooldownTime = 2f;
+        private float _dashCooldown;
+        private bool _isDashing;
+        private bool _canDash = true;
+
+        void Start() {
+            _canDash = true;
+        }
+
         // Update is called once per frame
         void Update() {
+            if (_isDashing) return;
             _movement.x = Input.GetAxisRaw("Horizontal");
             _movement.y = Input.GetAxisRaw("Vertical");
 
             _mousePos = Camera.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(_mousePos);
-        }
+
+            if (!_canDash) {
+                _dashCooldown -= Time.deltaTime;
+                if (_dashCooldown <= 0) {
+                    _canDash = true;
+                }
+            }
+            else {
+                if (!Input.GetKeyDown(KeyCode.Space)) return;
+                Dash();
+                _canDash = false;
+                _dashCooldown = _dashCooldownTime;
+            }
+        } 
 
         public void FixedUpdate() {
             Rb.MovePosition(Rb.position + _movement * MovementSpeed * Time.fixedDeltaTime);
@@ -24,6 +52,17 @@ namespace Assets.Scripts.PlayerScripts {
             var lookDir = _mousePos - Rb.position;
             var angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
             Rb.rotation = angle;
+        }
+
+        private void Dash() {
+
+            _canDash = false;
+            _isDashing = true;
+
+            Rb.position += _movement * _dashDistance;
+            _isDashing = false;
+
+            _canDash = true;
         }
     }
 }
